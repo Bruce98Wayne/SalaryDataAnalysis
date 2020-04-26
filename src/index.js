e7be87bfffb9de9350e792feb331497156d2d3f6
Bruce98Wayne <adorableakshat98@gmail.com>
2020-04-25 21:25:10 -0500

    Initialized react app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+const root = document.getElementById("root")
+
+if(root !== NULL) {
+    ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        root
+      );
+}
