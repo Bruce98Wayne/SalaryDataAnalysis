2d0c14d8766f6ab806921dcd4a4f1b5c65b625d6
LordVoldemort28 <rahul.prajapati90904@gmail.com>
2020-04-28 11:09:12 -0500

    Added sequelize

--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const db = require('./db/db')
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+db.authenticate()
+  .then(() => { 
+    console.log('Connected to the database successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  })
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
